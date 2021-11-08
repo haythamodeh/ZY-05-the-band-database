d13072825ce16fbc436157fa3d1572ae156e20a6
Haytham <haythamlion@outlook.com>
2021-11-08 13:56:11 -0800

    adds settings menu and adds app bar menu, adds setting fragment page

--- app/src/main/java/com/zybooks/thebanddatabase/ListFragment.java
+++ app/src/main/java/com/zybooks/thebanddatabase/ListFragment.java
@@ -1,15 +1,14 @@
 package com.zybooks.thebanddatabase;
 
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import androidx.annotation.NonNull;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 public class ListFragment extends Fragment {
@@ -18,73 +17,48 @@ public class ListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_list, container, false);
+        LinearLayout layout = (LinearLayout) rootView;
+        Button mSettingsbtn = new Button(new ContextThemeWrapper(getContext(), R.style.buttonStyle));
+        mSettingsbtn.setText("Settings");
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(0, 0, 0, 10);   // 10px bottom margin
 
-        // Click listener for the RecyclerView
-        View.OnClickListener onClickListener = itemView -> {
+        mSettingsbtn.setLayoutParams(layoutParams);
+        layout.addView(mSettingsbtn);
 
-            // Create fragment arguments containing the selected band ID
-            int selectedBandId = (int) itemView.getTag();
-            Bundle args = new Bundle();
-            args.putInt(DetailFragment.ARG_BAND_ID, selectedBandId);
+        mSettingsbtn.setOnClickListener(btn -> {
+            Navigation.findNavController(btn).navigate(R.id.settingsFragment);
+        });
 
-            // Replace list with details
-            Navigation.findNavController(itemView).navigate(R.id.show_item_detail, args);
-        };
+        // Create the buttons using the band names and ids from BandRepository
+        List<Band> bandList = BandRepository.getInstance(requireContext()).getBands();
+        for (Band band : bandList) {
+            Button button = new Button(getContext());
 
-        // Send bands to RecyclerView
-        RecyclerView recyclerView = rootView.findViewById(R.id.band_list);
-        List<Band> bands = BandRepository.getInstance(requireContext()).getBands();
-        recyclerView.setAdapter(new BandAdapter(bands, onClickListener));
-        DividerItemDecoration divider = new DividerItemDecoration(recyclerView.getContext(),
-                DividerItemDecoration.VERTICAL);
-        recyclerView.addItemDecoration(divider);
+            button.setLayoutParams(layoutParams);
+
+            // Display band's name on button
+            button.setText(band.getName());
+            button.setTag(band.getId());
+
+            // Navigate to detail screen when clicked
+            button.setOnClickListener(buttonView -> {
+                // Create fragment arguments containing the selected band ID
+                int selectedBandId = (int) buttonView.getTag();
+                Bundle args = new Bundle();
+                args.putInt(DetailFragment.ARG_BAND_ID, selectedBandId);
+
+                // Send band ID to DetailFragment
+                Navigation.findNavController(buttonView).navigate(R.id.show_item_detail, args);
+            });
+
+            // Add button to the LinearLayout
+
+            layout.addView(button);
+        }
 
         return rootView;
-    }
-
-    private class BandAdapter extends RecyclerView.Adapter<BandHolder> {
-
-        private final List<Band> mBands;
-        private final View.OnClickListener mOnClickListener;
-
-        public BandAdapter(List<Band> bands, View.OnClickListener onClickListener) {
-            mBands = bands;
-            mOnClickListener = onClickListener;
-        }
-
-        @NonNull
-        @Override
-        public BandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
-            return new BandHolder(layoutInflater, parent);
-        }
-
-        @Override
-        public void onBindViewHolder(BandHolder holder, int position) {
-            Band band = mBands.get(position);
-            holder.bind(band);
-            holder.itemView.setTag(band.getId());
-            holder.itemView.setOnClickListener(mOnClickListener);
-        }
-
-        @Override
-        public int getItemCount() {
-            return mBands.size();
-        }
-    }
-
-    private static class BandHolder extends RecyclerView.ViewHolder {
-
-        private final TextView mNameTextView;
-
-        public BandHolder(LayoutInflater inflater, ViewGroup parent) {
-            super(inflater.inflate(R.layout.list_item_band, parent, false));
-            mNameTextView = itemView.findViewById(R.id.band_name);
-
-        }
-
-        public void bind(Band band) {
-            mNameTextView.setText(band.getName());
-        }
     }
 }
--- app/src/main/java/com/zybooks/thebanddatabase/MainActivity.java
+++ app/src/main/java/com/zybooks/thebanddatabase/MainActivity.java
@@ -1,14 +1,21 @@
 package com.zybooks.thebanddatabase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private Menu mMenu;
+    private FragmentManager fm;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,5 +36,34 @@ public class MainActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return navController.navigateUp() || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar_menu, menu);
+        mMenu = menu;
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        // Determine which menu option was chosen
+        if (item.getItemId() == R.id.settingsMenu) {
+            showSettings(item);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showSettings(MenuItem item) {
+        fm = getSupportFragmentManager();
+        fm.beginTransaction()
+                .setReorderingAllowed(true)
+                .replace(R.id.nav_host_fragment, SettingsFragment.class, null)
+                .addToBackStack("SettingsFragment")
+                .commit();
+//        Navigation.findNavController((View) item).navigate(R.id.settingsFragment);
     }
 }
